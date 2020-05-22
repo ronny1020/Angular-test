fd9af46f792301ec50287bd349af627eb8a4074b
Ronny Hsieh <1020ronny@gmail.com>
2020-05-22 23:43:56 +0800

    ngFor test

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Todo } from '../../models/Todo'
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.sass']
+  styleUrls: ['./todo-list.component.sass'],
 })
 export class TodoListComponent implements OnInit {
+  todoList: Todo[]
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.todoList = [
+      { id: 1, title: 'Todo One', completed: false },
+      { id: 2, title: 'Todo Two', completed: false },
+      { id: 3, title: 'Todo Three', completed: false },
+    ]
   }
-
 }
