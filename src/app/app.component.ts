4cb7309851468d34f9a45b720b3b688b2850c42c
Ronny Hsieh <1020ronny@gmail.com>
2020-05-22 21:57:23 +0800

    todolist folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Angular test';
+  title:string = 'Angular test';
+
+  constructor(){
+    console.log('test')
+  }
 }
