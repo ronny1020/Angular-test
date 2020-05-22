20efcef206fbc5af89d7ed1941d45c578b6a8d0b
Ronny Hsieh <1020ronny@gmail.com>
2020-05-23 00:17:57 +0800

    boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { TodoListComponent } from './components/todo-list/todo-list.component'
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoListComponent, TodoItemComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
